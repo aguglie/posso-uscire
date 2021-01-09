6d8768922df035f4e007cd7d1d72601373226d17
EdoCap <edoardo.cappello@gmail.com>
2021-01-09 17:56:17 +0100

    feat: add translations for red zone rules

--- src/rules/zonaRossa.js
+++ src/rules/zonaRossa.js
@@ -3,19 +3,52 @@ import { ALL_REGIONS } from './Constants'
 const zonaRossa = {
   name: '🟥 Zona Rossa',
   details: [
-    '🚫 Bar, ristoranti chiusi tranne che per asporto.',
-    '🛍 Negozi e mercati chiusi ad esclusione di generi alimentati e prima necessità.',
-    '👭 Si può uscire (al massimo in coppia) per raggiungere amici e parenti una volta al giorno. Senza cambiare comune.',
-    '🛏 Coprifuoco dalle 22.00 alle 05.00.',
-    "📝 Serve l'autocertificazione per qualsiasi spostamento.",
+    {
+      'it': '🚫 Bar, ristoranti chiusi tranne che per asporto.',
+      'en': '🚫 Bar and resturants open just for take-away service.',
+    },
+    {
+      'it': '🛍 Negozi e mercati chiusi ad esclusione di generi alimentati e prima necessità.',
+      'en': '🛍 All the shops and markets are closed except for groceries and convinence stores.',
+    },
+    {
+      'it': '👭 Si può uscire (al massimo in coppia) per raggiungere amici e parenti una volta al giorno. Senza cambiare comune.',
+      'en': '👭 It is allowed to travel (not more than 2 people at a time) to friends and relatives once a day without changing municipality.',
+    },
+    {
+      'it': '🛏 Coprifuoco dalle 22.00 alle 05.00.',
+      'en': '🛏 Curfew from 10 PM to 5 AM.',
+    },
+    {
+      'it': '📝 Serve l\'autocertificazione per qualsiasi spostamento.',
+      'en': '📝 Self-certification is needed to justify any shift.',
+    },
   ],
   moreDetails: [
-    '👭 Si può uscire dal comune per comprovate esigenze di lavoro, di salute o situazione di necessità.',
-    '🏚 Consentito il rientro alla propria residenza, domicilio o abitazione ad esclusione delle seconde case fuori regione.',
-    '⛪ Consentito l\' accesso ai luoghi di culto e funzioni religiose.',
-    '🏃 Consentito svolgere attività sportiva (correre) esclusivamente all\'aperto ed individualmente.',
-    '🚶 Consentito svolgere attività motoria (camminare) in prossimità della propria abitazione, con mascherina.‍',
-    '✅ Aperte edicole, tabaccai, farmacie e parafarmacie.',
+    {
+      'it': '👭 Si può uscire dal comune per comprovate esigenze di lavoro, di salute o situazione di necessità.',
+      'en': '👭 Shifts across municipalities are allowed for work requirements, health needs, and situations of need.',
+    },
+    {
+      'it': '🏚 Consentito il rientro alla propria residenza, domicilio o abitazione ad esclusione delle seconde case fuori regione.',
+      'en': '🏚 The return to one\'s residence, domicile or home is allowed, with the exception of second homes outside the region.',
+    },
+    {
+      'it': '⛪ Consentito l\' accesso ai luoghi di culto e funzioni religiose.',
+      'en': '⛪ It is allowed to access places of worship and religious functions.',
+    },
+    {
+      'it': '🏃 Consentito svolgere attività sportiva (correre) esclusivamente all\'aperto ed individualmente.',
+      'en': '🏃 It is allowed to carry out sports activities (running) exclusively outdoors and individually.',
+    },
+    {
+      'it': '🚶 Consentito svolgere attività motoria (camminare) in prossimità della propria abitazione, con mascherina.‍',
+      'en': '🚶 It is allowed to carry out motor activities (walking) near your home, with the mask.',
+    },
+    {
+      'it': '✅ Aperte edicole, tabaccai, farmacie e parafarmacie.',
+      'en': '✅ Newsstand, tobacconists, pharmacies, and parapharmacies are open.',
+    },
   ],
 }
 
