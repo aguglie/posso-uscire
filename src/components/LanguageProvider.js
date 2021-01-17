654dc3ddbe7bc653d7604001cd3cdb7e34a442eb
Andrea Guglielmini <il.guglio95@gmail.com>
2021-01-17 22:46:03 +0100

    fix: default language is always italian.

--- src/components/LanguageProvider.js
+++ src/components/LanguageProvider.js
@@ -1,30 +1,10 @@
 import PropTypes from 'prop-types'
 import React, { createContext, useContext, useState } from 'react'
 
-const supportedLanguages = ['it', 'en']
-const defaultLanguage = 'en'
-
-function getLocale() {
-  if (window.navigator.languages) {
-    return window.navigator.languages[0]
-  }
-  return window.navigator.userLanguage || window.navigator.language
-}
-
-export function getLanguage() {
-  const locale = getLocale()
-  const language = locale.substr(0, 2)
-
-  if (supportedLanguages.includes(language)) {
-    return language
-  }
-  return defaultLanguage
-}
-
 const LanguageContext = createContext(null)
 
 export function LanguageProvider({ children }) {
-  const [language, setLanguage] = useState(getLanguage())
+  const [language, setLanguage] = useState('it')
 
   return (
     <LanguageContext.Provider value={[language, setLanguage]}>
