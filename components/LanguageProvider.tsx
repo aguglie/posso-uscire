bbb0f2c0b81f5245f96c99bd45d0ee5ac67ce393
Andrea Guglielmini <il.guglio95@gmail.com>
2021-01-25 20:27:21 +0100

    feat: italian as default language

--- components/LanguageProvider.tsx
+++ components/LanguageProvider.tsx
@@ -2,30 +2,11 @@ import React, {
   createContext,
   ReactElement,
   useContext,
-  useEffect,
   useState,
 } from "react";
 import PropTypes from "prop-types";
 
-const supportedLanguages = ["it", "en"];
 const defaultLanguage = "it";
-
-function getLocale() {
-  if (window.navigator.languages) {
-    return window.navigator.languages[0];
-  }
-  return (window.navigator as any).userLanguage || window.navigator.language;
-}
-
-export function getLanguage(): string {
-  const locale = getLocale();
-  const language = locale.substr(0, 2);
-
-  if (supportedLanguages.includes(language)) {
-    return language;
-  }
-  return defaultLanguage;
-}
 
 const LanguageContext = createContext<[string, (lang: string) => void]>([
   defaultLanguage,
@@ -34,9 +15,7 @@ const LanguageContext = createContext<[string, (lang: string) => void]>([
 
 export function LanguageProvider({ children }): ReactElement {
   const [language, setLanguage] = useState(defaultLanguage);
-  useEffect(() => {
-    setLanguage(getLanguage());
-  }, []);
+
   return (
     <LanguageContext.Provider value={[language, setLanguage]}>
       {children}
