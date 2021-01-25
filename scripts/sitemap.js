c6aac33fe56d88667ed495c9d4f3199e49cf6e63
Andrea Guglielmini <il.guglio95@gmail.com>
2021-01-25 20:25:11 +0100

    feature: automatic sitemap generation

---
+++ scripts/sitemap.js
@@ -0,0 +1,47 @@
+const globby = require("globby");
+const prettier = require("prettier");
+
+const now = new Date().toISOString();
+
+const domain = "https://possouscire.casa";
+
+const formatted = (sitemap) => prettier.format(sitemap, { parser: "html" });
+
+(async () => {
+  const pages = await globby([
+    // include
+    "./out/**/*.html",
+    // exclude
+    "!./out/404/**",
+    "!./out/_next/**",
+  ]);
+
+  const pagesSitemap = pages
+    .map((it) => it.replace(/^\.\/out(.+)index\.html/, "$1"))
+    .map(
+      (it) =>
+        "          <url>\n" +
+        "            <loc>" +
+        domain +
+        it +
+        "</loc>\n" +
+        "            <lastmod>" +
+        now +
+        "</lastmod>\n" +
+        "          </url>"
+    )
+    .join("\n");
+
+  const generatedSitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+    >
+      ${pagesSitemap}
+    </urlset>
+  `;
+
+  console.log(formatted(generatedSitemap));
+})();
